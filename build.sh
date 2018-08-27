551bb04de2342bd1cf38c6e5c61fc7e0b22bc053
Ismael Celis <ismaelct@gmail.com>
2018-08-27 12:33:28 +0100

    Comment

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
+# https://www.digitalocean.com/community/tutorials/how-to-build-go-executables-for-multiple-platforms-on-ubuntu-16-04
 mkdir -p builds
 env GOOS=linux GOARCH=386 go build -o builds/bootic_themes_backup.linux386
